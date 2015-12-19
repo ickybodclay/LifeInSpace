f4d052c2f4620bda3014948e3094acd9d5a2eb3c
Jason Petterson <jp@brokenshotgun.com>
2015-12-19 12:48:19 -0500

    initial commit

---
+++ android/src/com/brokenshotgun/lifeinspace/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.brokenshotgun.lifeinspace.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.brokenshotgun.lifeinspace.LifeInSpaceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LifeInSpaceGame(), config);
+	}
+}
