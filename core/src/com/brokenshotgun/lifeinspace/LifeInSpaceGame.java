6adff69b235d0c32445bbdc73cdcc3cb846811ec
Jason Petterson <jp@brokenshotgun.com>
2015-12-21 16:53:57 -0500

    removed overlap2d as it also appears to be incompatible with html
    Also it is just too frustratng to use in it's current state.

--- core/src/com/brokenshotgun/lifeinspace/LifeInSpaceGame.java
+++ core/src/com/brokenshotgun/lifeinspace/LifeInSpaceGame.java
@@ -3,34 +3,20 @@ package com.brokenshotgun.lifeinspace;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.uwsoft.editor.renderer.SceneLoader;
-import com.uwsoft.editor.renderer.utils.ItemWrapper;
 
 public class LifeInSpaceGame extends ApplicationAdapter {
-	private SceneLoader sceneLoader;
 	private Viewport viewport;
 
 	@Override
 	public void create () {
         viewport = new FitViewport(25f, 18.75f);
-		sceneLoader = new SceneLoader();
-        sceneLoader.loadScene("MainScene", viewport);
-
-        ItemWrapper root = new ItemWrapper(sceneLoader.getRoot());
-
-        Player player = new Player();
-        root.getChild("player").addScript(player);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        sceneLoader.engine.update(Gdx.graphics.getDeltaTime());
 	}
 }
--- core/src/com/brokenshotgun/lifeinspace/Player.java
+++ core/src/com/brokenshotgun/lifeinspace/Player.java
@@ -1,37 +1,7 @@
 package com.brokenshotgun.lifeinspace;
 
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.uwsoft.editor.renderer.components.TransformComponent;
-import com.uwsoft.editor.renderer.scripts.IScript;
-
 /**
  * Created by Jason on 12/21/2015.
  */
-public class Player implements IScript {
-    private Entity entity;
-    private TransformComponent transformComponent;
-
-    @Override
-    public void init(Entity entity) {
-        this.entity = entity;
-        transformComponent = entity.getComponent(TransformComponent.class);
-    }
-
-    @Override
-    public void act(float delta) {
-        transformComponent.x +=
-                Gdx.input.isKeyPressed(Input.Keys.LEFT) ? -1 :
-                Gdx.input.isKeyPressed(Input.Keys.RIGHT) ? 1 : 0;
-
-        transformComponent.y +=
-                Gdx.input.isKeyPressed(Input.Keys.UP) ? 1 :
-                Gdx.input.isKeyPressed(Input.Keys.DOWN) ? -1 : 0;
-    }
-
-    @Override
-    public void dispose() {
-
-    }
+public class Player {
 }
