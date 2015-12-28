f7cd70c9d23b0b91f97c80cca1a2414be2caab89
Jason Petterson <jp@brokenshotgun.com>
2015-12-28 13:20:23 -0500

    added 9 patch button background, added texture packer to desktop launcher

--- core/src/com/brokenshotgun/lifeinspace/LifeInSpaceGame.java
+++ core/src/com/brokenshotgun/lifeinspace/LifeInSpaceGame.java
@@ -2,21 +2,34 @@ package com.brokenshotgun.lifeinspace;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 public class LifeInSpaceGame extends ApplicationAdapter {
     private Stage stage;
     private Table table;
 
+    private AssetManager assetManager;
+    private String spriteAtlasFileName = "sprites.atlas";
+    private TextureAtlas spriteAtlas;
+
     @Override
     public void create() {
+        assetManager = new AssetManager();
+        assetManager.load(spriteAtlasFileName, TextureAtlas.class);
+        assetManager.finishLoading();
+
+        spriteAtlas = assetManager.get(spriteAtlasFileName);
+
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
@@ -24,18 +37,20 @@ public class LifeInSpaceGame extends ApplicationAdapter {
         table.setFillParent(true);
         stage.addActor(table);
 
-        table.setDebug(true);
+        //table.setDebug(true);
 
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        ImageTextButton.ImageTextButtonStyle style = new ImageTextButton.ImageTextButtonStyle();
         style.font = new BitmapFont();
         style.fontColor = Color.WHITE;
         style.overFontColor = Color.BLUE;
-        style.downFontColor = Color.CHARTREUSE;
+        style.downFontColor = Color.DARK_GRAY;
+        style.up = new NinePatchDrawable(spriteAtlas.createPatch("button_normal"));
+        style.down = new NinePatchDrawable(spriteAtlas.createPatch("button_pressed"));
 
-        TextButton testButton = new TextButton("Test", style);
-        testButton.pad(10, 40, 10, 40);
+        ImageTextButton testButton = new ImageTextButton("Test", style);
+        testButton.pad(10);
         testButton.addListener(new InputListener() {
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("touchDown 1");
                 return false;
             }
--- desktop/src/com/brokenshotgun/lifeinspace/desktop/DesktopLauncher.java
+++ desktop/src/com/brokenshotgun/lifeinspace/desktop/DesktopLauncher.java
@@ -2,16 +2,25 @@ package com.brokenshotgun.lifeinspace.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.brokenshotgun.lifeinspace.LifeInSpaceGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		RunTexturePacker();
 
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 600;
         config.resizable = false;
 
 		new LwjglApplication(new LifeInSpaceGame(), config);
+	}
+
+	private static void RunTexturePacker() {
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxWidth = 512;
+		settings.maxHeight = 512;
+		TexturePacker.process(settings, "../../images/sprites", "../assets", "sprites");
 	}
 }
