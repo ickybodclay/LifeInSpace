dc8c9ba9437727a773de1b32213f1c56f6f10c87
Jason Petterson <jp@brokenshotgun.com>
2016-01-05 21:43:42 -0500

    added sprites for rover minigame

--- core/src/com/brokenshotgun/lifeinspace/screens/RoverScreen.java
+++ core/src/com/brokenshotgun/lifeinspace/screens/RoverScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -13,6 +14,10 @@ import com.brokenshotgun.lifeinspace.LifeInSpaceGame;
 
 public class RoverScreen implements Screen {
     private final LifeInSpaceGame game;
+
+    private final String spriteAtlasFile = "sprites.atlas";
+    private TextureAtlas spriteAtlas;
+    private final Color martianRed = Color.valueOf("ac3232");
 
     private Stage stage;
 
@@ -22,6 +27,11 @@ public class RoverScreen implements Screen {
 
     @Override
     public void show() {
+        game.getAssetManager().load(spriteAtlasFile, TextureAtlas.class);
+        game.getAssetManager().finishLoading();
+
+        spriteAtlas = game.getAssetManager().get(spriteAtlasFile);
+
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
@@ -37,7 +47,7 @@ public class RoverScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClearColor(martianRed.r, martianRed.g, martianRed.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act(delta);
